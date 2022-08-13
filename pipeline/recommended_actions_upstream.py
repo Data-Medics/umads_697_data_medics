146ff95a827f20a91ad88423f9df1e69a4a617b0
Tyler Zupan <tylerzupan@tylers-mbp.lan>
2022-08-13 14:01:22 -0400

    updated code for blog data sample

--- pipeline/recommended_actions_upstream.py
+++ pipeline/recommended_actions_upstream.py
@@ -1,4 +1,6 @@
 import yaml
+import datetime
+import pandas as pd
 
 from tools_zupan import get_hashtags, query_tweets
 
@@ -15,7 +17,7 @@ query = '(wildfire OR flood OR earthquake donate evacuate volunteer)'
 disaster_types = ["wildfire", "flood", "earthquake"]
 action_types = ["volunteer", "donate", "evacuate"]
 language = 'en'
-limit = 5000
+limit = 500
 credentials_file = "credentials.yaml"
 
 # +
@@ -32,20 +34,49 @@ with open(credentials_file, 'r') as stream:
 
 bearer_token = credentials['twitter_bearer_token']
 
-df = query_tweets(bearer_token=bearer_token, query=query + ' lang:' + language, limit=limit)
+# df = query_tweets(bearer_token=bearer_token, query=query + ' lang:' + language, limit=limit)
 
+skip_interval_hours = 6
+periods_one_week = 24
+period_end = datetime.datetime.now()
+period_delta = datetime.timedelta(hours=skip_interval_hours)
+
+full_df = None
+
+for _ in range(periods_one_week):
+    df = query_tweets(bearer_token=bearer_token, query=query + ' lang:' + language, 
+                      limit=limit,  end_time=period_end)
+    if full_df is None:
+        full_df = df
+    else:
+        full_df = pd.concat([full_df, df])
+    period_end -= period_delta
+
+
+# +
+# skip_interval_hours = 6
+# periods_one_week = 24
+# period_end = datetime.datetime.now()
+# period_delta = datetime.timedelta(hours=skip_interval_hours)
+
+# full_df = None
+
+# for _ in range(periods_one_week):
+#     print(period_end)
+#     period_end -= period_delta
+
+# +
 # hash the tweet text to count unique tweets
-df["tweet_hash"] = df["tweet_text"].apply(lambda a: hash(a) % 100000000)
+full_df["tweet_hash"] = full_df["tweet_text"].apply(lambda a: hash(a) % 100000000)
 
 # counts by tweet
-df["tweet_count"] = df.groupby("tweet_hash").transform("count")["tweet_text"]
+full_df["tweet_count"] = full_df.groupby("tweet_hash").transform("count")["tweet_text"]
 
-df_slim = df.groupby("tweet_hash").first()
+df_slim = full_df.groupby("tweet_hash").first()
 
 # df_slim.to_csv(product['file'], index=False)
+print(df_slim.shape)
 df_slim.to_csv("output/twitter_actions.csv", index=False)
-
-
 # -
 
 
