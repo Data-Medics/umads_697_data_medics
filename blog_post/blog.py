c8d27c0f30fb587bfdb36041a1d84a33526d6eff
[Robert Gillin] <[rjgillin@gmail.com]>
2022-08-15 13:29:03 -0700

    streamlit work

--- blog_post/blog.py
+++ blog_post/blog.py
@@ -66,7 +66,7 @@ A sample of the data can be seen here:
     
     st.subheader("Real Time Data")
     """
-    However, giving people critical information about a disaster that happened several years ago is of limited value.  For our project to truly help, we needed to apply our models and analysis to live Twitter data around current natural disasters.  To do this we utilized [Tweepy](https://www.tweepy.org/) and wrote several functions that pulled real time Twitter based on our disaste specific search terms which we then used as inputs to our model.
+    However, giving people critical information about a disaster that happened several years ago is of limited value.  For our project to truly help, we needed to apply our models and analysis to live Twitter data around current natural disasters.  To do this we utilized [Tweepy](https://www.tweepy.org/) and wrote several functions that pulled real time Twitter based on our disaster specific search terms which we then used as inputs to our model.
     """
     st.subheader("Project Plan")
     """
@@ -83,6 +83,75 @@ A sample of the data can be seen here:
     
 with tab_2:
     st.header(tabs_list[1])
+    st.subheader("Overview")
+    """
+        In order to provide actionable recommendations during times of disaster, we needed a way to efficiently query recent and relevant disaster tweets from twitter.
+        To do so we developed a function to extract tweets from the Twitter API based on a query of keywords. The keywords we used to build this query were a direct
+    product of the Latent Dirichlet Allocation (LDA) topic modeling analysis we conducted on the HumAid disaster tweets dataset.
+    """
+    st.subheader("Topic Modeling Exploratory Analysis")
+    st.markdown("**Token Exploration**")
+    """
+        To gain some initial familiarity with the disaster tweet text, we performed some simple token exploration.
+    More specifically we looked at how many tokens were composed of word characters versus tokens comprised of non-word characters.
+    Tweet text can be very messy, and we would eventually need to decide how to handle hashtags, symbols, retweets and mentions.
+    """
+    st.subheader("Model Design")
+    st.markdown("**Text Pre-Processing**")
+    """
+        After a few initial iterations, it became clear that the success and performance of our topic model
+    would largely depend on how well we were able to pre-process the disaster tweet text. This naturally became an iterative process
+    as we adjusted the following parameters and inputs, measuring topic quality at each iteration:
+    1. Stopword removal
+    2. Tokenization technique (single tokens, bigrams, trigams)
+    3. Splitting of compound words
+    4. Inclusion and exclusion of hashtags, twitter handles (@user), web addresses, non-ascii characters
+    5. Word length
+    """
+    st.markdown("**Lemmatization & Vectorization**")
+    """
+        Lemmatization is the process of reducing various inflectional forms of a word to it's base form. We chose lemmatization
+    over stemming because we wanted to retain the morphological structure of words, believing this would be
+    a more robust way to capture topics. Stemming is a more crude approach in which the end or the beginning of a word is cut
+    off to reduce the word to it's root. 
+    
+    We used the spaCy [Lemmatizer](https://spacy.io/api/lemmatizer) pipeline component to transform our pre-processed tweet text into it's lemmatized form.
+    One challenge we encountered when initially building out our topic model, was that our topics were being focused around
+    specific events and locations. This was undesirable as we wanted our topics to be generalizable to future disasters. spaCy's 
+    lemmatzier gave us the flexibility to filter out unwanted part-of-speech tags. We chose to only include
+    nouns, adjectives, verbs and adverbs in our final lemmatized text. In some cases there were still location names that slipped
+    through our pre-processing steps. For these unique cases, we added these words to our list of stopwords.
+    
+    
+    For our text vectorization, we needed to convert our lemmatized text into a bag-of-words representation which the LDA model
+    could use. We used the sklearn [CountVectorizer](https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html) 
+    class to produce a sparse representation of token counts. The CountVectorizer class conveniently let us set our ngram_range to (1,2) along 
+    with filtering out our pre-defined stopwords.
+    """
+    st.markdown("**Model Selection and Training**")
+    """
+        We used the sklearn [LatentDirichletAllocation](https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.LatentDirichletAllocation.html)
+    class for our topic model. Initially we had built the topic model on the entire disaster tweet dataset. However, it became clear that
+    the topics we were generating were suboptimal. More specifically, the topics were blended across the various disaster types leading to poor
+    interpretability. To address this, we decided to train a separate topic model for each disaster type. While this introduced additional
+    algorithmic complexity, it turned out to be the right approach, as our topics became much more concise and easily interpretable. 
+    
+    The single key parameter for the LDA model is the "n_components" parameter. After some initial experimentation,
+    it seemed that an n_components value between 2-4 was returning relatively concise topics. Eventually we would perform a more
+    robust hyperparameter tuning exercise to systematically estimate the optimal number of topics, but initially we set n_components = 3,
+    so we could continue with our development work. From here, we fine-tuned various aspects of our topic modeling pipeline using log-liklihood
+    scores, perplexity scores and general topic interpretability to guide our refinements to the model. 
+    """
+    st.markdown("**Hyperparameter Tuning**")
+    """  
+        The final step before deploying the LDA model was to estimate the optimal number of topics for each disaster type LDA model. We developed a script
+    that performs a grid search exersice, varying the number of topics from 2-6 across each disaster type LDA model. We then computed the
+    log-liklihood and coherence scores for each configuration. When calculating coherence, we used the 'u_mass' measurement as opposed to
+    the 'c_v' measurement. The 'c_v' measurement is known to produce the most reliable results, but opted to use the 'u_mass' measurement
+    to speed up the computation. The full grid search took approximately 4 hrs to run. See figure A below for the results.
+    """
+    st.image("blog_post/blog_data/topic_model_gridsearch_results.png", caption="")
+    st.subheader("Topic Modeling Results")
 
 with tab_3:
     st.header(tabs_list[2])
